14ff0a1ac81820f09d0880c363242399a1038929
seoa <andrew.d.seo@gmail.com>
2020-06-18 11:04:51 -0700

    starting point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const MovingObject = require("./moving_object.js");
+
+window.MovingObject = MovingObject;
+window.addEventListener('DOMContentLoaded', (event) => {
+    let ctx = document.getElementById("game-canvas").getContext('2d');
+
+})
